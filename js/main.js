a37293578092f9ea266d811503d22912fb716953
Davide Benazzi <benazzi.davide@gmail.com>
2020-11-06 15:26:38 +0100

    Message

--- js/main.js
+++ js/main.js
@@ -3,4 +3,9 @@ Descrizione:
 Stampare a schermo un messaggio all'interno di un h1, utilizzando i data.
 BONUS aggiungere un'immagine presa anch'essa da un data (url dell'immagine)
 */
-console.log('js ok!!!');
+const root = new Vue ({
+  el: '#root',
+  data: {
+    message: 'Hello World!!!',
+  },
+});
